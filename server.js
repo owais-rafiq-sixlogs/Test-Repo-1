e12b762ba94455c4552bc4dbc281361ea71b827a
Sixlogs Project Team <syed.zaki@sixlogs.com>
2026-09-01 11:46:04 +0000

    feat: server code added

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const PORT = 3002;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello World</h1>");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
